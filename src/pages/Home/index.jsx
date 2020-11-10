f7cf6af7211f1f41c1031851bb00f1c8e8b0ccfb
cledson <cledman@gmail.com>
2020-11-10 10:09:26 -0300

    Home updated

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useState } from 'react';
+import TextField, {Input} from '@material/react-text-field';
+import logo from '../../assets/logo.svg';
+import { Container, Search } from './styles';
 
 const Home = () =>{
+    const [inputValue, setInputValue] = useState('');
     return(
-        <Container>asdasdasd</Container>
+        <Container>
+            <Search >
+                <img src={logo} alt="logo from Restaurant"/>
+                <TextField
+                    label='Pesquisar'
+                    outlined
+                  //  onTrailingIconSelect={() => this.setState({value: ''})}
+                    //trailingIcon={<MaterialIcon role="button" icon="delete"/>}
+                    >
+                    <Input
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)} 
+                    />
+                </TextField>                
+            </Search>
+        </Container>
     )
 }
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,4 +1,15 @@
 import styled from 'styled-components';
-export const Container = styled.div`
-   // background-color: ${(props) => props.theme.colors.primary};
+export const Container = styled.aside`
+    background-color: ${(props) => props.theme.colors.background};
+    height:100vh;
+    overflow-y:auto;
+    width:360px;
+`;
+
+export const Search = styled.section`
+    background-color:#ffffff;
+    display:flex;
+    flex-direction: column;
+    justify-content:center;
+    padding:16px;
 `;
